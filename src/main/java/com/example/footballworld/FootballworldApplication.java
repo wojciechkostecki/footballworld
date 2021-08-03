58c6efba98a16b2f6c2eefaed51ae00c3a1e414a
48531 <wt.kostecki@gmail.com>
2021-08-03 11:27:34 +0200

    add all files

---
+++ src/main/java/com/example/footballworld/FootballworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footballworld/FootballworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
