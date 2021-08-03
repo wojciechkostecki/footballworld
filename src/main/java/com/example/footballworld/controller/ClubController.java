efe9610bfd416443526de5def0c169217b6090d0
48531 <wt.kostecki@gmail.com>
2021-08-03 19:03:31 +0200

    Creating a controller for the Club entity

---
+++ src/main/java/com/example/footballworld/controller/ClubController.java
@@ -0,0 +1,47 @@
+package com.example.footballworld.controller;
+
+import com.example.footballworld.model.Club;
+import com.example.footballworld.service.ClubService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clubs")
+public class ClubController {
+    private final ClubService clubService;
+
+    public ClubController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Club> createClub(@RequestBody Club club) {
+        Club savedClub = clubService.save(club);
+        return new ResponseEntity<>(savedClub, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Club>> getAllClubs() {
+        return ResponseEntity.ok(clubService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Club> getClub(@PathVariable Long id) {
+        return ResponseEntity.ok(clubService.findById(id).get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Club> updateClub(@PathVariable Long id, @RequestBody Club club) {
+        return ResponseEntity.ok(clubService.updateClub(id, club));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClub(@PathVariable Long id) {
+        clubService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
