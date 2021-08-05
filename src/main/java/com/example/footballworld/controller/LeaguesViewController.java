57744454994ef700871c53c851e909e5d9dbc2de
48531 <wt.kostecki@gmail.com>
2021-08-05 20:51:38 +0200

    Improvement url

--- src/main/java/com/example/footballworld/controller/LeaguesViewController.java
+++ src/main/java/com/example/footballworld/controller/LeaguesViewController.java
@@ -21,29 +21,29 @@ public class LeaguesViewController {
         return "leagues";
     }
 
-    @GetMapping("/showNewLeagueForm")
+    @GetMapping("/league/create")
     public String showNewLeagueForm(Model model) {
         League league = new League();
         model.addAttribute("league", league);
         return "new_league";
     }
 
-    @PostMapping("/saveLeague")
+    @PostMapping("/league/save")
     public String saveLeague(@ModelAttribute("league") League league) {
         leagueService.save(league);
-        return "redirect:/";
+        return "redirect:/view";
     }
 
-    @GetMapping("/showFormForUpdate/{id}")
+    @GetMapping("/league/update/{id}")
     public String showFormForUpdate(@PathVariable Long id, Model model) {
         League league = leagueService.findById(id).get();
         model.addAttribute("league", league);
         return "update_league";
     }
 
-    @GetMapping("/deleteLeague/{id}")
+    @GetMapping("/league/delete/{id}")
     public String deleteLeague(@PathVariable Long id) {
         this.leagueService.delete(id);
-        return "redirect:/";
+        return "redirect:/view";
     }
 }
