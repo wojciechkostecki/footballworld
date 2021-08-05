e88169e7c38971b7392a29190f94cbd5ec45511b
48531 <wt.kostecki@gmail.com>
2021-08-05 23:23:39 +0200

    Configuration Docker Bean

--- src/main/java/com/example/footballworld/FootballWorldApplication.java
+++ src/main/java/com/example/footballworld/FootballWorldApplication.java
@@ -2,7 +2,16 @@ package com.example.footballworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -12,4 +21,21 @@ public class FootballWorldApplication {
         SpringApplication.run(FootballWorldApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.footballworld.controller"))
+                .build().apiInfo(createApiInfo());
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("Football World",
+                "Some custom description of API.",
+                "1.00",
+                null,
+                new Contact("Wojciech Kostecki", "https://www.linkedin.com/in/wojciech-kostecki-7816411a4/", "wt.kostecki@gmail.com"),
+                null, null, Collections.emptyList());
+    }
 }
